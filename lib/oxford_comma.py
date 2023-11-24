46bb2cf3bec0b164cf7e2b33149d60404a052274
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-24 14:48:13 +0300

    all tests passed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0: # Check if the list is empty
+        return "" # If it is, return an empty string
+    elif len(items) == 1: # Check if the list contains only 1 item
+        return items[0] # If so, return the single item
+    elif len(items) == 2: # Check if the list contains 2 items
+        return f"{items[0]} and {items[1]}" # If it does, join the 2 items with the word "and" in between them
+    else:
+        oxford_str = ", ".join(items[:-1]) # If the list contains more than 2 items, join all the items except the last one with a comma and a space 
+        return f"{oxford_str}, and {items[-1]}" # Then, add the last item to the string with the word "and" before it, and return the resulting string
